602706e24843229b71a5437da952391844cb4310
bodya <lavajover@gmail.com>
2025-02-06 16:03:36 +0300

    feat: add service layer

---
+++ storage-service/internal/service/service.go
@@ -0,0 +1,25 @@
+package service
+
+import (
+	"github.com/LavaJover/storage-master/storage-service/internal/models"
+	"github.com/LavaJover/storage-master/storage-service/internal/repo"
+)
+
+
+type StorageService struct{
+	*repo.BoxRepo
+	*repo.CellRepo
+	*repo.StorageRepo
+}
+
+func (service *StorageService) CreateStorage (storage *models.Storage) error {
+	return service.StorageRepo.InsertStorage(storage)
+}
+
+func (service *StorageService) AddCell (cell *models.Cell) error {
+	return service.CellRepo.InsertCell(cell)
+}
+
+func (service *StorageService) AddBox (box *models.Box) error {
+	return service.BoxRepo.InsertBox(box)
+}
