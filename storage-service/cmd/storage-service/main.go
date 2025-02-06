ae04744fd6d312017ae8cf1bfa56d77e3b8acbbc
bodya <lavajover@gmail.com>
2025-02-06 14:14:10 +0300

    feat: add config file reader

--- storage-service/cmd/storage-service/main.go
+++ storage-service/cmd/storage-service/main.go
@@ -1,6 +1,11 @@
 package main
 
-func main(){
+import (
+	"fmt"
+	"github.com/LavaJover/storage-master/storage-service/internal/config"
+)
 
-	
+func main(){
+	cfg := config.MustLoad()
+	fmt.Println(cfg)
 }
---
+++ storage-service/internal/config/config.go
@@ -0,0 +1,38 @@
+package config
+
+import (
+	"log"
+	"os"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+type StorageConfig struct {
+	Env        string `yaml:"env" env-required:"true"`
+	GRPCServer `yaml:"grpc_server" env-required:"true"`
+}
+
+type GRPCServer struct {
+	Host string `yaml:"host" env-default:"localhost" env-required:"true"`
+	Port string `yaml:"port" env-default:"50051" env-required:"true"`
+}
+
+func MustLoad() *StorageConfig {
+	var cfg StorageConfig
+
+	configPath := os.Getenv("STORAGE_CONFIG_PATH")
+
+	if configPath == ""{
+		panic("STORAGE_CONFIG_PATH required")
+	}
+
+	if _, err := os.Stat(configPath); err != nil{
+		log.Fatal("storage-service config file was not found")
+	}
+
+	if err := cleanenv.ReadConfig(configPath, &cfg); err != nil{
+		log.Fatal("failed to read config")
+	}
+
+	return &cfg
+}
