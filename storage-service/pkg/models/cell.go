91b8d5cfb1c40f286db15b6abf182d8d25757310
bodya <lavajover@gmail.com>
2025-02-07 19:43:01 +0300

    feat: add FKs to models

--- storage-service/pkg/models/cell.go
+++ storage-service/pkg/models/cell.go
@@ -5,5 +5,7 @@ import "gorm.io/gorm"
 type Cell struct {
 	gorm.Model
 	Name string `gorm:"column:name;unique;not null"`
-	StorageID uint `gorm:"column:storage_id;not null"`
+	StorageID uint `gorm:"column:storage_id;not null;index"`
+
+	Boxes []Box `gorm:"foreignKey:CellID"`
 }
