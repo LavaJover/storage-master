a937b52da024890e5732ae6e5c5d7d5749278c70
bodya <lavajover@gmail.com>
2025-02-07 18:00:00 +0300

    feat: init service layer

--- storage-service/internal/service/service.go
+++ storage-service/internal/service/service.go
@@ -12,14 +12,17 @@ type StorageService struct{
 	*repo.StorageRepo
 }
 
+// Storage service methods
 func (service *StorageService) CreateStorage (storage *models.Storage) error {
 	return service.StorageRepo.InsertStorage(storage)
 }
 
+// Cell service methods
 func (service *StorageService) AddCell (cell *models.Cell) error {
 	return service.CellRepo.InsertCell(cell)
 }
 
+// Box service methods
 func (service *StorageService) AddBox (box *models.Box) error {
 	return service.BoxRepo.InsertBox(box)
 }
